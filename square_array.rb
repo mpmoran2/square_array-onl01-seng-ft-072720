c898276e0ff5d5ff1fb433f3458a3a03ff63c9c3
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-07-29 16:53:57 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  newArray=[]
+     array.each do |numbers|
+     numbers=numbers**2 
+     newArray<<numbers
+  end
+  return newArray
 end
+     
+     
+ 
+ 
+
